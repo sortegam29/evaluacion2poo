13459f9ca5c8ff63631be401ba2d03a3bca85d42
sortegam29 <sebastian.ortega29@inacapmail.cl>
2021-11-16 02:29:51 -0300

    segunda subida

--- evaluacion2.py
+++ evaluacion2.py
@@ -1,29 +1,58 @@
-#test
+from typing import List
+
+listaEstudiantes = list()
 
 class Estudiante:
-#falta asignar id
-    def __init__(self,nombres,apellidos,rut,direccion):
-        self.nombres = nombres
-        self.apellidos = apellidos
-        self.rut = rut
-        self.direccion = direccion
+#falta asignar id según diagrama
+    def __init__(self):
+        self.nombres = ("")
+        self.apellidos = ("")
+        self.rut = ("")
+        self.direccion = ("")
 
-    def getNombres(self):
-        return self.nombres
-    def getApellidos(self):
-        return self.apellidos
-    def getRut(self):
-        return self.rut
-    def getDireccion(self):
-        return self.direccion
 
-    def imprimirEtudiante(self):
-        print("\nNombres: " +self.getNombres()+"\nApellidos: "+self.getApellidos()+"\nRut: "+self.getRut()+"\nDireccion: "+self.getDireccion())
+#Primer menu
 
-nombres = input("ingrese nombres: ")
-apellidos = input("Ingrese apellidos: ")
-rut = input("Ingrese Rut: ")
-direccion = input("Ingrese direccion: ")
+def menu():
+    seleccion=0
+    while seleccion!= 4:
+        print("---Menu de vio----") 
+        print("seleccione una opcion")
+        print("........................")
+        print("1. registro de alumno")
+        print("2. mostrar")
+        print("3. buscar")
+        print("4. salir")
+        seleccion = int(input("Elija una opcion: "))
+        if seleccion == 1:
+            registrar()
+        if seleccion == 2:
+            mostrar()
+        if seleccion == 3:
+            buscar()
+        if seleccion == 4:
+            salir()
 
-e = Estudiante(nombres,apellidos,rut,direccion)
-e.imprimirEtudiante()
+def registrar():
+    print ("esta este es registro")
+    estudiante = Estudiante()
+    estudiante.nombres=input("ingrese nombres: ")
+    estudiante.apellidos=input("ingrese apellidos: ")
+    estudiante.rut=input("ingrese rut: ")
+    estudiante.direccion=input("ingrese direccion: ")
+    listaEstudiantes.append(estudiante)
+
+
+def mostrar():
+    print ("esta es una muestra")
+    for estudiante in listaEstudiantes:
+        print( estudiante.nombres, estudiante.apellidos, estudiante.rut, estudiante.direccion )
+        
+def buscar():
+    print ("esto busca")
+def salir():
+    print ("chao")
+
+
+
+menu()
